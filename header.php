5d2be35743054815bdfce96483bbcf4c1e4cbc79
Miguel Peixe <miguel@cardume.art.br>
2017-03-28 19:19:03 -0300

    header/hero design experiments

--- header.php
+++ header.php
@@ -8,19 +8,44 @@
 </head>
 <body <?php body_class(); ?>>
   <header id="masthead">
+    <div class="container">
+        <div class="six columns">
+            <h1><a href="<?php echo home_url('/'); ?>">Mapas Culturais</a></h1>
+        </div>
+        <div class="six columns">
+          <input class="search u-pull-right" type="text" placeholder="Busque qualquer coisa">
+          <nav id="mastnav" class="u-pull-right">
+            <a href="page.html">Mapas Culturais</a>
+            <a href="blog.html">Blog</a>
+            <a href="page.html">Parceiros</a>
+            <a href="support.html">Suporte</a>
+            <a href="page.html">Contato</a>
+          </nav>
+        </div>
+    </div>
+    <section id="hero">
       <div class="container">
-          <div class="six columns">
-              <h1><a href="<?php echo home_url('/'); ?>">Mapas Culturais</a></h1>
+        <div class="seven columns">
+          <div class="intro-text">
+            <p class="big">Software livre para gestão cultural e mapeamento colaborativo.</p>
+            <p><a class="button" href="support.html">Suporte</a><a class="button button-primary" href="page.html">Saiba mais</a></p>
           </div>
-          <div class="six columns">
-            <input class="search u-pull-right" type="text" placeholder="Busque qualquer coisa">
-            <nav class="u-pull-right" id="mastnav">
-              <a href="page.html">Mapas Culturais</a>
-              <a href="blog.html">Blog</a>
-              <a href="page.html">Parceiros</a>
-              <a href="support.html">Suporte</a>
-              <a href="page.html">Contato</a>
-            </nav>
+        </div>
+        <div class="five columns">
+          <div class="network">
+            <div class="intro-numbers row">
+              <p class="number">30</p>
+              <p class="number-label">plataformas <span>na rede</span></p>
+            </div>
+            <p><a class="button u-full-width" href="installations.html">Conheça as instalações</a></p>
+            <div class="ptns">
+              <div class="ptn ptn-1">
+                <a href="#">Conheça as instalações</a>
+              </div>
+              <div class="ptn ptn-2"></div>
+              <div class="ptn ptn-3"></div>
+            </div>
           </div>
-      </div>
+        </div>
+    </section>
   </header>
--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php get_header(); ?>
 <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-
+<section id="content">
+  <div class="container">
+    <div class="nine columns">
+      <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent elementum ut ligula in accumsan.</h2>
+    </div>
+    <div class="row">
+      <div class="six columns">
+        <p>Proin egestas ante sapien, et venenatis felis luctus a. In interdum facilisis augue quis tempor. Mauris luctus lobortis diam, quis aliquet velit sollicitudin quis. Mauris mattis elit ac justo commodo pulvinar. Quisque porta libero massa, ultrices imperdiet nunc tempor ac. Quisque accumsan lectus ac orci interdum, id pretium nulla tempus. Vestibulum lorem velit, ultricies et faucibus et, lacinia non felis. Duis congue nec odio ut bibendum. Nunc rutrum, velit non feugiat accumsan, nisl arcu fringilla nulla, in porta justo felis ac purus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit odio non neque auctor, eget ornare nunc hendrerit. Integer sit amet fermentum urna. Aliquam auctor magna tristique pretium tincidunt. Phasellus dictum nunc purus, eget varius urna varius sit amet.</p>
+      </div>
+    </div>
+  </div>
+<section>
 <?php endwhile; endif; ?>
 <?php get_footer(); ?>
