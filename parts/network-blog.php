8eebf3337110644a0d54dd6f5ff28b8efb00e748
Miguel Peixe <miguel.peixe@aparabolica.com.br>
2017-08-02 19:53:42 -0300

    decrease amount of network posts (avoid blank line on bottom of section)

--- parts/network-blog.php
+++ parts/network-blog.php
@@ -74,30 +74,6 @@
         </div>
       </div>
     </article>
-    <hr class="dark" />
-    <article class="row">
-      <div class="author network-author">
-        <div class="author-thumb">
-          <img src="http://lorempixel.com/100/100/?2" />
-        </div>
-        <h4>SPCultura</h4>
-      </div>
-      <div class="network-post-content">
-        <h3>Mauris mattis elit ac justo commodo pulvinar. Quisque porta libero massa.</h3>
-        <div class="meta">
-          <p class="date">
-            <span class="fa fa-clock-o"></span>
-            10 de abril de 2017
-          </p>
-          <p class="comments">
-            <a href="#">
-              <span class="fa fa-comments-o"></span>
-              10 comentários
-            </a>
-          </p>
-        </div>
-      </div>
-    </article>
   </div>
   <p><a class="button block">Conheça o blog da rede</a></p>
 </section>
