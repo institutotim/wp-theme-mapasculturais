97822f233eb7656e86d9759dca64913b108b3221
Miguel Peixe <miguel.peixe@aparabolica.com.br>
2017-08-08 17:00:19 -0300

    update network page

--- network.php
+++ network.php
@@ -30,7 +30,7 @@
               </div>
               <div class="six columns">
                 <div class="page-header-text connect-border connect-right">
-                  <p>Mapas Culturais foi criado pelo Instituto TIM em parceria com a Secretaria de Cultura do Município de São Paulo, a primeira a adotar a plataforma, em 2014. Atualmente, a solução está em operação no Ministério da Cultura, em estados e municípios de todas as regiões do Brasil e até no exterior.</p>
+                  <p>Mapas Culturais foi criado pelo <a href="http://institutotim.org.br" rel="external" target="_blank">Instituto TIM</a> em parceria com a Secretaria de Cultura do Município de São Paulo, a primeira a adotar a plataforma, em 2014. Atualmente, a solução está em operação no Ministério da Cultura, em estados e municípios de todas as regiões do Brasil e até no exterior. Em 2015, Mapas Culturais passou a ser a plataforma oficial do Sistema Nacional de Informações e Indicadores Culturais (SNIIC), sendo o sistema oficial para mapeamento colaborativo e gestão da cultura do Ministério da Cultura.</p>
                 </div>
                 <div class="network-numbers connect-border connect-right">
                   <hr />
