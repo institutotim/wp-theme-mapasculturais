6fe78e0b2fbf7e236b5170d9d4e6f50d8af214d4
Maurilio Atila <cabelotaina@gmail.com>
2018-01-23 16:29:43 -0200

    added sidebar on news page

--- archive.php
+++ archive.php
@@ -55,7 +55,8 @@
       </div>
       <div class="three columns offset-by-one">
         <div id="sidebar" class="sidebar regular-sidebar connect-border connect-right">
-          <div class="widget text-widget">
+          <?php dynamic_sidebar('news-pmc') ?>
+         <!--  <div class="widget text-widget">
             <h2>Exemplo de widget de texto</h2>
             <div class="widget-content">
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod lectus a cursus dictum. Nulla at metus id elit volutpat ornare ut quis nisl. Sed pellentesque leo in massa ornare, eu tincidunt nibh cursus. Nulla vestibulum, enim in vulputate imperdiet, felis arcu dignissim ipsum, at laoreet urna lectus at metus. Phasellus vitae massa ac ligula placerat sagittis. Ut lobortis purus in neque vestibulum, quis tincidunt ipsum posuere. Etiam vel pellentesque justo. Aliquam semper id purus eu cursus.</p>
@@ -90,7 +91,7 @@
                 </a>
               </li>
             </ul>
-          </div>
+          </div> -->
         </div>
       </div>
     </div>
---
+++ inc/sidebars.php
@@ -0,0 +1,24 @@
+<?php
+
+class PMC_Sidebars {
+
+  function __construct() {
+    add_action('init', array($this, 'register_sidebars'));
+  }
+
+  function register_sidebars() {
+    $args = array(
+      'name'          => __( 'News', 'pmc' ), //linha do tempo
+      'id'            => 'news-pmc', 
+      'description'   => '',
+      'class'         => 'widget text-widget',
+      'before_widget' => '<p id="%1$s" class="widget %2$s">',
+      'after_widget'  => '</p>',
+      'before_title'  => '<h2>',
+      'after_title'   => '</h2>' );
+
+    register_sidebar( $args );
+  }
+}
+
+$pmc_sidebars = new PMC_Sidebars();
