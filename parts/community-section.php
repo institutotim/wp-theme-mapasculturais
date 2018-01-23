3a26861f7f2d4d8bd2248e3d22159b71f4b6f032
Maurilio Atila <cabelotaina@gmail.com>
2018-01-23 14:53:09 -0200

    add github link to footer

--- parts/community-section.php
+++ parts/community-section.php
@@ -18,7 +18,7 @@
       <div class="four columns">
         <div class="buttons">
           <p>
-            <a  href="https://github.com/hacklabr/mapasculturais"class="button block">
+            <a  href="https://github.com/hacklabr/mapasculturais" class="button block">
               <span class="fa fa-github"></span>
               GitHub
             </a>
