414417a7c04335c22e2932b7d1caa22ba4cb4fd2
Maurilio Atila <cabelotaina@gmail.com>
2018-01-26 21:20:18 -0200

    footer menu

--- footer.php
+++ footer.php
@@ -2,42 +2,15 @@
   <div class="container">
     <div class="eight columns">
       <nav id="footnav">
-        <ul class="nav">
-          <li>
-            <a href="#">Sobre</a>
-            <ul>
-              <li><a href="#">Lorem ipsum</a></li>
-              <li><a href="#">Dolor sit amet</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="<?php echo get_post_type_archive_link('pmc_timeline_item'); ?>">Linha do Tempo</a>
-            <ul>
-              <li><a href="#">Proin egestas</a></li>
-              <li><a href="#">Mauris luctus lobortis</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">Contato</a>
-            <ul>
-              <li><a href="#">Quisque accumsan</a></li>
-              <li><a href="#">Vestibulum lorem</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">Parceiros</a>
-            <ul>
-              <li><a href="#">Lorem ipsum</a></li>
-              <li><a href="#">Dolor sit amet</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="<?php echo get_option('github_url') ?>">
-              <span class="fa fa-github"></span>
-              GitHub
-            </a>
-          </li>
-        </ul>
+          <?php wp_nav_menu(array(
+            "theme_location" => "footer",
+            "menu_class" => "nav",
+            "menu_id" => "",
+            "container_class" => "",
+            "container_id" => ""
+            )
+          ); 
+          ?>
       </nav>
       <hr/>
       <p class="credits">Software livre desenvolvido pelo <a href="http://institutotim.org.br/" target="_blank" title="Instituto TIM" rel="external"><img alt="Instituto TIM" src="<?php echo get_template_directory_uri(); ?>/img/institutotim.png" /></a>.</p>
