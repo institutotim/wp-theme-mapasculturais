29dc3e812f1d642e23b0fb9a74a6372937854ad6
miguelpeixe <miguel@cardume.art.br>
2018-02-28 19:52:49 -0300

    use delibera theming for new pauta

---
+++ delibera/delibera_nova_pauta.php
@@ -0,0 +1,38 @@
+<?php
+global $deliberaThemes;
+get_header();
+?>
+
+<div id="nova-pauta">
+	<header class="page-header">
+    <div class="container">
+      <div class="twelve columns">
+        <hr>
+      </div>
+    </div>
+    <div class="container">
+      <div class="eight columns">
+        <div class="page-header-content has-icon no-text">
+					<div class="row">
+						<span class="page-icon">
+	              <span class="fa fa-group"></span>
+	            </span>
+	          <h2>Nova pauta</h2>
+					</div>
+        </div>
+      </div>
+    </div>
+	</header>
+	<div id="content">
+		<div class="container">
+			<div class="eight columns offset-by-two">
+				<?php
+				// chama o formulário de nova pauta
+				if(is_user_logged_in())
+				include $deliberaThemes->themeFilePath('form-nova-pauta.php');
+				?>
+			</div>
+	</div><!-- #content -->
+</div><!-- #container -->
+
+<?php get_footer(); ?>
