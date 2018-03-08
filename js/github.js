b15de47a0f64f922ce3b9e6731dab35dace34ca8
Maurilio Atila <cabelotaina@gmail.com>
2018-03-08 11:54:41 -0300

    update author github view

--- js/github.js
+++ js/github.js
@@ -1,29 +1,70 @@
 (function($) {
   document.addEventListener("DOMContentLoaded", function(event) {
 
-    $('.spaces-chart').hide()
-    $('.projects-chart').hide()
-    $('.events-chart').hide()
+    // initial hide all subttitles
 
     $('#spaces-result').hide()
     $('#projects-result').hide()
     $('#events-result').hide()
 
+    var type = $("#types option:selected").val();
+
     $('#types').change(function(){
+      //hide and show subtitle
+      $('#'+type+'-result').hide();
+      var aux = $("#types option:selected").val();
+      $('#'+aux+'-result').show();
+      type = aux;
 
-      $('.spaces-chart').hide()
-      $('.agents-chart').hide()
-      $('.projects-chart').hide()
-      $('.events-chart').hide()
+      // load data
 
-      $('#spaces-result').hide()
-      $('#agents-result').hide()
-      $('#projects-result').hide()
-      $('#events-result').hide()
+      $chart = $('.global-chart').highcharts();
 
-      $("."+$("#types option:selected").val()+'-chart').show();
-      $("#"+$("#types option:selected").val()+'-result').show();
+      var data = []
+
+      data = {
+        'commit_count': 0,
+        'chart_data': []
+      };
+
+      for(date in instanceData[type]){
+        data.chart_data.push([
+          new Date(date).toISOString(),
+          instanceData[type][date]
+        ]);
+      }
+
+      data.commit_count = data.chart_data[0][1];
+
+      data.chart_data.reverse();
+
+
+      $chart.series[0].setData(data.chart_data);
+
+      if (type === 'spaces'){
+        type_pt = 'espaços';
+      }
+      else if (type === 'projects'){
+        type_pt = 'projetos';
+      }
+      else if (type === 'agents') {
+        type_pt = 'agentes';
+      }
+      else if (type === 'events'){
+        type_pt = 'eventos';
+      }
+
+
+      $chart.tooltip.formatter = function() {
+        var label = '<span class="label"><b>Atividade recente</b></span><br/>';
+        return '<span class="gh-tooltip">' + label + '<span class="date">' + moment(this.key).format('L') + '</span> - <span class="total">' + this.y + ' '+type_pt+'</span></span>';
+      };
+
     });
+
+
+
+
 
     if (typeof instanceData === 'undefined'){
 
@@ -106,93 +147,88 @@
 
     } else {
       var data = []
-      for(type in instanceData){
 
-        data[type] = {
-          'commit_count': 0,
-          'chart_data': []
-        };
+      data = {
+        'commit_count': 0,
+        'chart_data': []
+      };
 
-        for(date in instanceData[type]){
-          data[type].chart_data.push([
-            new Date(date).toISOString(),
-            instanceData[type][date]
-          ]);
-        }
-
-        data[type].commit_count = data[type].chart_data[0][1];
-
-
-         data[type].chart_data.reverse();
-
-        console.log(data[type]);
-        console.log('.'+type+'-chart');
-        $chart = $('.'+type+'-chart');
-
-        var name = type+'-chart';
-
-        if($chart.length) {
-          $chart.highcharts({
-            chart: {
-              type: 'spline',
-              backgroundColor: 'transparent',
-              height: 150,
-              spacingBottom: 0,
-              spacingTop: 0,
-              spacingLeft: 0,
-              spacingRight: 0,
-            },
-            renderTo: name,
-            title: false,
-            subtitle: false,
-            tooltip: {
-              enabled: true,
-              followPointer: true,
-              backgroundColor: 'transparent',
-              formatter: function() {
-                var label = '<span class="label"><b>Atividade recente</b></span><br/>';
-                return '<span class="gh-tooltip">' + label + '<span class="date">' + moment(this.key).format('L') + '</span> - <span class="total">' + this.y + ' '+type+'</span></span>';
-              },
-              borderRadius: 0,
-              shadow: false,
-              style: {
-                color: '#000'
-              }
-            },
-            legend: {
-              enabled: false
-            },
-            credits: {
-              enabled: false
-            },
-            xAxis: {
-              type: 'datetime',
-              dateTimeLabelFormats: {
-                month: '%e. %b',
-                year: '%b'
-              },
-              title: 'Date',
-              tickInterval: 7 * 24 * 3600 * 1000, // 1 week
-              tickWidth: 0,
-              visible: false
-            },
-            yAxis: {
-              min: 0,
-              title: 'Commits',
-              visible: false
-            },
-            series: [{
-              lineWidth: 10,
-              marker: {
-                enabled: false
-              },
-              name: 'Commits',
-              color: '#e94b80',
-              data: data[type].chart_data
-            }]
-          });
-        }
+      for(date in instanceData['agents']){
+        data.chart_data.push([
+          new Date(date).toISOString(),
+          instanceData['agents'][date]
+        ]);
       }
+
+      data.commit_count = data.chart_data[0][1];
+
+      data.chart_data.reverse();
+
+      $chart = $('.global-chart');
+
+      if($chart.length) {
+        $chart.highcharts({
+          chart: {
+            type: 'spline',
+            backgroundColor: 'transparent',
+            height: 150,
+            spacingBottom: 0,
+            spacingTop: 0,
+            spacingLeft: 0,
+            spacingRight: 0,
+          },
+          renderTo: 'global-chart',
+          title: false,
+          subtitle: false,
+          tooltip: {
+            enabled: true,
+            followPointer: true,
+            backgroundColor: 'transparent',
+            formatter: function() {
+              var label = '<span class="label"><b>Atividade recente</b></span><br/>';
+              return '<span class="gh-tooltip">' + label + '<span class="date">' + moment(this.key).format('L') + '</span> - <span class="total">' + this.y + ' '+type+'</span></span>';
+            },
+            borderRadius: 0,
+            shadow: false,
+            style: {
+              color: '#000'
+            }
+          },
+          legend: {
+            enabled: false
+          },
+          credits: {
+            enabled: false
+          },
+          xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: {
+              month: '%e. %b',
+              year: '%b'
+            },
+            title: 'Date',
+            tickInterval: 7 * 24 * 3600 * 1000, // 1 week
+            tickWidth: 0,
+            visible: false
+          },
+          yAxis: {
+            min: 0,
+            title: 'Commits',
+            visible: false
+          },
+          series: [{
+            lineWidth: 10,
+            marker: {
+              enabled: false
+            },
+            name: 'Commits',
+            color: '#e94b80',
+            data: data.chart_data
+          }]
+        });
+
+      }
+
     }
   });
 })(jQuery);
