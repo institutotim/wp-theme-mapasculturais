0b924af5b2720be20961661cf3562e9264e346b5
Vitor George <vitor.george@aparabolica.com.br>
2018-03-13 11:33:34 +0000

    Revert changes of e37e8e6589bb290c07ea33c99988516e416be865

--- inc/tutorials/widget.php
+++ inc/tutorials/widget.php
@@ -60,10 +60,18 @@ class tutorials_widget extends WP_Widget {
 	// Widget Backend 
 	public function form( $instance ) {
 		if ( isset( $instance[ 'title' ] ) ) {
-		$title = $instance[ 'title' ];
+			$title = $instance[ 'title' ];
 		}	else {
-		$title = __( 'New title', 'pmc' );
+			$title = __( 'New title', 'pmc' );
 		}
+		// Widget admin form
+		?>
+
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<?php 
 	}
      
 	// Updating widget replacing old instances with new
