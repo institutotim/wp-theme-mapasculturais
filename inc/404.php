c210ca2abec561b1254a49e5608e413c84c69f6f
Vitor George <vitor.george@aparabolica.com.br>
2018-03-13 14:38:58 +0000

    Use site_url() function in translation string #30

--- inc/404.php
+++ inc/404.php
@@ -9,7 +9,9 @@
       <div class="twelve columns">
         <div class="page-header-content no-text has-icon row">
           <h2><i class="fa fa-meh-o"></i><?php _e('Página não encontrada ', 'pmc'); ?></h2>
-          <p class="page-description"><?php __( 'Return to the <a href="/">main page</a> or perform a new search.', 'pmc' ); ?></p>
+          <p class="page-description">
+            <?php printf(__( 'Return to the <a href=%s>main page</a> or perform a new search.', 'pmc' ), site_url()); ?>
+          </p>
         </div>
       </div>
     </div>
