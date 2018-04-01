3a36a9473bc77ffc2bbc308397c19116619ffb10
Maurilio Atila <cabelotaina@gmail.com>
2018-04-01 20:18:16 -0300

    update community section

--- parts/community-section.php
+++ parts/community-section.php
@@ -18,13 +18,13 @@
       <div class="four columns">
         <div class="buttons">
           <p>
-            <a target="_blank" href="<?php echo get_option('theme_mods_mapasculturais')['github'] ?>" class="button block">
+            <a target="_blank" href="<?php echo get_option('theme_mods_mapasculturais')['github']; ?>" class="button block">
               <span class="fa fa-github"></span>
               GitHub
             </a>
           </p>
           <p>
-            <a target="_blank" href="<?php echo get_community_url() ?>" class="button button-primary block">
+            <a target="_blank" href="/comunidade" class="button button-primary block">
               <span class="fa fa-star"></span>
               Participe da discussão
             </a>
